0ea3cb96647167536b28307054526dc3674ed1d9
Dev Chauhan <124146292+DevChauhan5@users.noreply.github.com>
2023-12-01 07:25:45 +0000

    Project - Jivana | Day 2

---
+++ daily-life-tracker/javascript/script.js
@@ -0,0 +1,16 @@
+var tl = gsap.timeline();
+
+tl.from('.nav img', {
+    y: -200,
+    duration: .5,
+})
+
+tl.from('.nav-mid', {
+    y: -200,
+    duration: .5,
+})
+
+t1.from('#signin-btn', {
+    y: -200,
+    duration: .5,
+})
